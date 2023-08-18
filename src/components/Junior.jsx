77266e4823c4b399684f82f2934699459d88b15e
JuniorPacheco <junior.pacheco.356@gmail.com>
2023-08-17 23:01:29 -0500

    changes junior

--- src/components/Junior.jsx
+++ src/components/Junior.jsx
@@ -1,6 +1,9 @@
 const Junior = () => {
   return (
-    <div>Junior</div>
+    <div>
+      <img src="/gatoJunior.jpg" alt="" />
+      <h2>Junior Pacheco</h2>
+    </div>
   )
 }
 export default Junior
