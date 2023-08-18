251f7d0572c14a4070431c72a6696706211a01d2
6CROONUX9 <jerchiton.97@gmail.com>
2023-08-17 23:02:48 -0400

    primera parte de mi aporte al proyecto

--- src/components/Danilo.jsx
+++ src/components/Danilo.jsx
@@ -1,6 +1,21 @@
 const Danilo = () => {
   return (
-    <div>Danilo</div>
+     <article className="bg-white rounded-md overflow-hidden ligthTheme darkTheme">
+        {/* seccion superior de  mi card*/}
+        <header className="h-[160px] ligthTheme">
+            <img className="h-full w-full object-cover object-center" src="/danilo.jpg" alt="" />
+        </header>
+
+        {/* seccion inferior de  mi card*/}
+        <section className="p-2 py-4">
+        <h3 className="font-bold mb-2 text-lg line-clamp-1">Danilo Florez</h3>
+        <ul>
+            <li><span className="font-semibold">Corregimiento:</span>Samore</li>
+            <li><span className="font-semibold">Municipio:</span>Toledo</li>
+            <li><span className="font-semibold">Departamento:</span>N.de.S</li>
+        </ul>
+        </section>
+    </article>
   )
 }
 export default Danilo
